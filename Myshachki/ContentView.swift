d18f0143c6b947fffd3a50709c0b4f45a08c4bf7
Vladyslav Pidborskyi <vladyslav.pidborskyi@gmail.com>
2026-06-08 15:55:42 +0200

    Initial Commit

---
+++ Myshachki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Myshachki
+//
+//  Created by Vlad Pidborskyi on 08.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Myshachki/MyshachkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyshachkiApp.swift
+//  Myshachki
+//
+//  Created by Vlad Pidborskyi on 08.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyshachkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
